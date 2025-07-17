8eb849d4d1dcef620e7c30e5dfb61bde890ac765
kremzil <43149900+kremzil@users.noreply.github.com>
2025-07-18 00:45:55 +0200

    Improve designer integration

--- filerobot-designer.php
+++ filerobot-designer.php
@@ -2,35 +2,60 @@
 /*
 Plugin Name: Filerobot Web2Print Designer (Popup)
 Description: Открывает Filerobot Editor в новом окне и сохраняет дизайн в заказ WooCommerce.
-Version: 2.0
+Version: 2.1
 Author: Viktor
 */
 
 define('FD_PLUGIN_URL', plugin_dir_url(__FILE__));
 define('FD_PLUGIN_PATH', plugin_dir_path(__FILE__));
+define('FD_VERSION', '2.1');
+
+// Add product setting to enable the designer
+add_action('woocommerce_product_options_general_product_data', function() {
+    global $product_object;
+    $value = $product_object ? $product_object->get_meta('_fd_enable_designer') : 'no';
+    woocommerce_wp_checkbox([
+        'id'          => '_fd_enable_designer',
+        'label'       => __('Enable Filerobot Designer', 'filerobot-designer'),
+        'desc_tip'    => true,
+        'description' => __('Allow customers to design this product.', 'filerobot-designer'),
+        'value'       => $value,
+    ]);
+});
+
+add_action('woocommerce_admin_process_product_object', function($product) {
+    $enabled = isset($_POST['_fd_enable_designer']) && wc_string_to_bool(wp_unslash($_POST['_fd_enable_designer'])) ? 'yes' : 'no';
+    $product->update_meta_data('_fd_enable_designer', $enabled);
+});
 
 add_action('wp_enqueue_scripts', function() {
     if (is_product()) {
-        $image_url = get_the_post_thumbnail_url(get_the_ID(), 'full');
-        wp_enqueue_script('fd-editor', FD_PLUGIN_URL . 'assets/js/editor.js', ['jquery'], null, true);
-        wp_localize_script('fd-editor', 'fd_ajax', [
-            'ajax_url' => admin_url('admin-ajax.php'),
-            'nonce'    => wp_create_nonce('fd_nonce'),
-            'image_url' => $image_url,
-            'popup_url' => FD_PLUGIN_URL . 'popup/index.html'
-        ]);
+        $enabled = get_post_meta(get_the_ID(), '_fd_enable_designer', true);
+        if ($enabled === 'yes') {
+            $image_url = esc_url(get_the_post_thumbnail_url(get_the_ID(), 'full'));
+            wp_enqueue_script('fd-editor', FD_PLUGIN_URL . 'assets/js/editor.js', ['jquery'], FD_VERSION, true);
+            wp_localize_script('fd-editor', 'fd_ajax', [
+                'ajax_url'  => admin_url('admin-ajax.php'),
+                'nonce'     => wp_create_nonce('fd_nonce'),
+                'image_url' => $image_url,
+                'popup_url' => esc_url(FD_PLUGIN_URL . 'popup/index.html'),
+            ]);
+        }
     }
 });
 
 add_action('woocommerce_after_add_to_cart_button', function() {
-    echo '<button id="open-designer" type="button" class="button alt">Vytvoriť dizajn</button>';
-    echo '<input type="hidden" id="fd-design-url" name="fd-design-url" value="">';
+    $enabled = get_post_meta(get_the_ID(), '_fd_enable_designer', true);
+    if ($enabled === 'yes') {
+        echo '<button id="open-designer" type="button" class="button alt">' . esc_html__('Vytvoriť dizajn', 'filerobot-designer') . '</button>';
+        echo '<input type="hidden" id="fd-design-url" name="fd-design-url" value="">';
+    }
 });
 
 add_action('woocommerce_checkout_create_order_line_item', function($item, $cart_item_key, $values, $order) {
     $design_url = WC()->session->get('fd_design_file');
     if ($design_url) {
-        $item->add_meta_data('Dizajn zákazníka', $design_url);
+        $item->add_meta_data('Dizajn zákazníka', esc_url($design_url));
         WC()->session->__unset('fd_design_file');
     }
 }, 10, 4);
@@ -44,7 +69,7 @@ function fd_save_design() {
         wp_send_json_error('No image provided.');
     }
 
-    $image_data = $_POST['image'];
+    $image_data = sanitize_textarea_field($_POST['image']);
     $decoded = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $image_data));
     $filename = 'design-' . time() . '.png';
     $upload = wp_upload_bits($filename, null, $decoded);
